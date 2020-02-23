04a4659f6f9e08f79ec532cefe62549946a86375
pintugithub10 <49206754+pintugithub10@users.noreply.github.com>
2020-02-24 00:40:42 +0530

    Update Person.java

--- src/main/java/com/hibernet/onetoone/entities/Person.java
+++ src/main/java/com/hibernet/onetoone/entities/Person.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-@Entity
+//@Entity
 @Table(name="person_table")
 public class Person implements Serializable{
 	
